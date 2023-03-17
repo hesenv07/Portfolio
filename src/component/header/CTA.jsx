6e87ceec27376e571ec007ddc8c0ca51bfae16b4
hesenv07 <arifhesenv@gmail.com>
2023-03-18 01:27:00 +0400

    update

--- src/component/header/CTA.jsx
+++ src/component/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from './../../assets/CV.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download="ds" className="btn">Download CV</a>
+      <a href={CV} download="Arif Hasanov - CV" className="btn">Download CV</a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
     </div>
   );
